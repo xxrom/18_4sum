ca092f751cdd71391cec46a90911b8a3a610b166
nchernyshov <xpom55@mail.ru>
2021-01-10 20:39:01 +0300

    ?solved? O(N*N * N) / memory O(N) without repeat optimization

--- main.py
+++ main.py
@@ -3,27 +3,45 @@ from typing import List
 
 class Solution:
 
-  def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-    size = len(nums)
+  def twoSum(self, nums, startIndex, target, c, d):
+    # time O(N)
+    # memory O(N)
+    # a + b = target
+    # a = target - b
 
-    ans = {}
+    seen = set()
+
+    for i in range(startIndex, len(nums)):
+
+      diff = target - nums[i]
+
+      if diff in seen:
+        print('found solution', c, d, diff, nums[i])
+        self.ans.add((c, d, diff, nums[i]))
+
+      seen.add(nums[i])
+
+  def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    # time O(N*N)
+    # memory O(N)
+
+    # a + b + c + d = target
+
+    size = len(nums)
+    self.ans = set()
 
     for i in range(size):
       for j in range(i + 1, size):
-        for k in range(j + 1, size):
-          for e in range(k + 1, size):
-            if i != j and i != k and i != e and j != k and j != e and k != e:
-              if (nums[i] + nums[j] + nums[k] + nums[e]) == target:
-                print(i, j, k, e)
-                values = [nums[i], nums[j], nums[k], nums[e]]
-                values.sort()
+        c = nums[i]
+        d = nums[j]
 
-                currentIndex = ','.join([str(v) for v in values])
-                print(currentIndex)
+        twoSumTarget = target - c - d
+        startIndex = j + 1
+        self.twoSum(nums, startIndex, twoSumTarget, c, d)
 
-                ans[currentIndex] = values
+    self.ans = [list(x) for x in list(self.ans)]
 
-    return list(ans.values())
+    return list(self.ans)
 
 
 my = Solution()
@@ -35,3 +53,32 @@ trueAns = [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
 ans = my.fourSum(n, t)
 
 print("ans", ans, ans == trueAns)
+'''
+a + b + c + d = target
+
+a + b = target - c - d
+
+[1, 0, -1, 0, -2, 2]
+[-2, -1, 0, 0,1, 2]
+
+a + b + c + e = t
+
+a + b = t - c - e
+
+a + b = t - c
+
+{}
+-2, 2
+
+{-2, 2}
+-1, 1
+
+{-2, 2, -1, 1}
+0, 0
+0 + 0 = 0 - c - e
+a + b - t = -c - e
+
+c + (a + b - t) = -e
+
+# find c / -e in { -2, 2, -1, 1}
+'''
