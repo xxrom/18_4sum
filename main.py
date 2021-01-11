96f96f7093746f9cba3f37708ba10e23ee1d9631
nchernyshov <xpom55@mail.ru>
2021-01-11 23:58:59 +0300

    solved: ~980ms (~40%) / 14.3mb (~75%) O(N*N*N) two for loop with twoSum() function

--- main.py
+++ main.py
@@ -3,69 +3,55 @@ from typing import List
 
 class Solution:
 
-  def twoSum(self, nums, startIndex, target, c, d):
-    # time O(N)
-    # memory O(N)
+  def twoSum(self, startIndex, target, c, d):
     # a + b = target
-    # a = target - b
-
     seen = set()
 
-    for i in range(startIndex, len(nums)):
-      if i > startIndex and nums[i - 1] == nums[i]:
-        continue
+    for i in range(startIndex, self.size):
 
-      diff = target - nums[i]
+      diff = target - self.nums[i]
 
       if diff in seen:
-        self.found += 1
-        print('found solution', self.found, ' / ', c, d, diff, nums[i])
-        self.ans.add((c, d, diff, nums[i]))
+        # print('found solution', c, d, diff, self.nums[i])
+        self.ans.add((c, d, diff, self.nums[i]))
 
-      seen.add(nums[i])
+      seen.add(self.nums[i])
 
   def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-    # time O(N*N)
-    # memory O(N)
-
-    # a + b + c + d = target
 
     nums.sort()
+    self.nums = nums
 
-    print(nums)
-
-    size = len(nums)
+    self.size = len(self.nums)
     self.ans = set()
-    self.found = 0
 
-    for i in range(size):
-      if i > 0 and nums[i - 1] == nums[i]:
+    for i in range(self.size - 3):
+      # Skip same values for first letter
+      if i > 0 and self.nums[i - 1] == self.nums[i]:
         continue
 
-      for j in range(i + 1, size):
-        if j > i + 1 and nums[j - 1] == nums[j]:
+      for j in range(i + 1, self.size - 2):
+        # Skip same values for second letter
+        if j > i + 1 and self.nums[j - 1] == self.nums[j]:
           continue
 
-        c = nums[i]
-        d = nums[j]
+        c = self.nums[i]
+        d = self.nums[j]
 
         twoSumTarget = target - c - d
         startIndex = j + 1
-        self.twoSum(nums, startIndex, twoSumTarget, c, d)
 
-    self.ans = [list(x) for x in list(self.ans)]
+        self.twoSum(startIndex, twoSumTarget, c, d)
 
-    return list(self.ans)
+    # convert to list from List[set(int)]
+    return [list(setItem) for setItem in list(self.ans)]
 
 
 my = Solution()
-n = [
-    1, 1, 1, 1, -100, 1, -100, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, -1, 0, -2, 2
-]
-t = 0
 
-trueAns = [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+n = [-2, -1, -1, 1, 1, 2, 2]
+t = 0
+trueAns = [[-2, -1, 1, 2], [-1, -1, 1, 1]]
 
 ans = my.fourSum(n, t)
 
@@ -82,20 +68,10 @@ a + b + c + e = t
 
 a + b = t - c - e
 
-a + b = t - c
 
-{}
--2, 2
-
-{-2, 2}
--1, 1
-
-{-2, 2, -1, 1}
-0, 0
-0 + 0 = 0 - c - e
-a + b - t = -c - e
-
-c + (a + b - t) = -e
-
-# find c / -e in { -2, 2, -1, 1}
+for (i) first letter (c)
+  for (j) second letter (e)
+    twoSum () {
+      faind a + b = target (target = t - c - e)
+    }
 '''
